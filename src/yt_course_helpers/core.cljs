66a151f078942b732ce0dfa53ceb062aac481459
Thomas Sojka <thsojka@web.de>
2020-04-20 18:08:05 +0200

    Use googleapis to fetch yt data

--- src/yt_course_helpers/core.cljs
+++ src/yt_course_helpers/core.cljs
@@ -1,67 +1,49 @@
 (ns yt-course-helpers.core
-  (:require ["node-fetch" :as fetch]
+  (:require ["googleapis" :as googleapis]
             [clojure.string :as s]
+            [goog.object :as o]
             [cljs.core.async :refer [go go-loop <!]]
             [cljs.core.async.interop :refer [<p!]]
             [yt-course-helpers.fs :as fs]
             [yt-course-helpers.auth :refer [authorize]]))
 
-
-(defn query-params [params]
-  (s/join "&" (map (fn [[key value]] (str (name key) "=" value)) params)))
-
-(defn GET
-  ([endpoint]
-   (-> (fetch (str "https://www.googleapis.com/youtube/v3/" endpoint)
-              (clj->js {:headers {:Content-Type "Application/json"
-                                  :Authorization (str "Bearer " (authorize))}}))
-       (.then #(.json %))
-       (.then #(js->clj % :keywordize-keys true))))
-  ([endpoint params]
-   (GET (str endpoint "?" (query-params params)))))
-
-(defn load-my-playlists []
-  (GET "playlists" {:part "contentDetails" :mine true}))
-
-(defn load-playlist-items [params]
-  (GET "playlistItems" params))
+(defn list-yt [entity params]
+  (go (js->clj (.-data (<p! (.list
+                             (o/get (.youtube (.-google googleapis) "v3") entity)
+                             (clj->js (assoc params :auth (authorize))))))
+               :keywordize-keys true)))
 
 (defn load-video-ids-of-playlist [playlist-id]
-  (go-loop [video-ids [] pageToken nil]
-    (let [{:keys [items nextPageToken]}
-          (<p! (load-playlist-items {:part "snippet,contentDetails"
-                                     :playlistId playlist-id
-                                     :pageToken pageToken}))]
+  (go-loop [video-ids [] pageToken ""]
+    (let [{:keys [items nextPageToken]} (<! (list-yt "playlistItems"
+                                                  {:part "snippet,contentDetails"
+                                                   :playlistId playlist-id
+                                                   :pageToken pageToken}))
+          all-ids (concat video-ids (map (comp :videoId :contentDetails) items))]
       (if nextPageToken
-        (recur (concat video-ids (map (comp :videoId :contentDetails) items))
-               nextPageToken)
-        (concat video-ids (map (comp :videoId :contentDetails) items))))))
-
-(defn convert-videos [videos]
-  (map (fn [video]
-         {:title (get-in video [:snippet :title])
-          :description (get-in video [:snippet :description])})
-       (:items videos)))
+        (recur all-ids nextPageToken)
+        all-ids))))
 
 (defn write-videos-file [videos]
   (fs/write-file "resources/videos.org"
               (s/join
                "\n"
                (map
-                (fn [{:keys [title description]}]
+                (fn [{{:keys [title description]} :snippet}]
                   (str "* " title "\n" description))
-                videos))))
+                (:items videos)))))
 
 (defn write-my-playlist-to-org-file []
   (go
     (try
-      (let [my-playlists (<p! (load-my-playlists))
+      (let [my-playlists (<! (list-yt "playlists" {:part "contentDetails" :mine true}))
             playlist-id (first (map :id (:items my-playlists)))
             video-ids (<! (load-video-ids-of-playlist playlist-id))
-            videos (<p! (GET (str "videos?part=snippet&id="
-                                  (s/join "," video-ids))))]
-        (write-videos-file (convert-videos videos)))
+            videos (<! (list-yt "videos" {:part "snippet" :id (s/join "," video-ids)}))]
+        (write-videos-file videos))
       (catch js/Error err (prn (ex-cause err))))))
+
+
 
 
 
